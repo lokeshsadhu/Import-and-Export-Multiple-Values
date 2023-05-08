5a66905b334bd0033dcd90eb62962c644684d116
lokeshsadhu <132827865+lokeshsadhu@users.noreply.github.com>
2023-05-08 19:33:02 +0530

    Update exportMultipleValues.mjs

--- exportMultipleValues.mjs
+++ exportMultipleValues.mjs
@@ -1,2 +1,5 @@
-export const a = 5;
-export const b = 10;
+export let myArray = ["camel", 265, true, "5.6"];
+export let bulb = { watts: 10, type: "LED" };
+export function multiplyByFour(num) {
+  return num * 4;
+}
